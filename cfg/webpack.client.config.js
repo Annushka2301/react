c76bb6bb8352e81287bb4a09de435625f83d08da
Anna <morozovaannuska@mail.ru>
2022-03-20 09:58:35 +0300

    css modules

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -7,38 +7,54 @@ const IS_DEV = NODE_ENV === 'development';
 const IS_PROD = NODE_ENV === 'production';
 
 function setupDevtool() {
-    if (IS_DEV) return 'eval';
-    if (IS_PROD) return false;
+  if (IS_DEV) return 'eval';
+  if (IS_PROD) return false;
 }
 
 module.exports = {
-    mode: NODE_ENV ? NODE_ENV : 'development',
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-        alias: {
-            'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
-        }
-    },
-    entry: [
-        path.resolve(__dirname, '../src/client/index.jsx'),
-        'webpack-hot-middleware/client?path=http:localhost:3001/static/__webpack_hmr',
-    ],
-    output: {
-        path: path.resolve(__dirname, '../dist/client'),
-        filename: 'client.js',
-        publicPath: '/static/',
-    },
-    module: {
-        rules: [{
-            test: /\.[tj]sx?$/,
-            use: ['ts-loader']
-        }]
-    },
-    devtool: setupDevtool(),
-    plugins: IS_DEV 
-    ? [
-        new CleanWebpackPlugin(),
-        new HotModuleReplacementPlugin(),
-    ] 
-    : [],
+  mode: NODE_ENV || 'development',
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    alias: {
+      'react-dom': IS_DEV ? '@hot-loader/react-dom' : 'react-dom',
+    }
+  },
+  entry: [
+    path.resolve(__dirname, '../src/client/index.jsx'),
+    'webpack-hot-middleware/client?path=//localhost:3001/static/__webpack_hmr',
+  ],
+  output: {
+    path: path.resolve(__dirname, '../dist/client'),
+    filename: 'client.js',
+    publicPath: '//localhost:3001/static/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.[tj]sx?$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', {
+            loader:  'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIndentName: '[name]__[local]--[hash:base64:5]',
+              }
+            }
+          }
+        ],
+      }
+    ]
+  },
+  devtool: setupDevtool(),
+  plugins: IS_DEV
+  ? [
+    new CleanWebpackPlugin(),
+    new HotModuleReplacementPlugin(),
+  ]
+  : [],
 };
--- dist/server/server.js
+++ dist/server/server.js
@@ -7,7 +7,6 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/server/indexTemplate.js":
@@ -16,6 +15,7 @@
   \*************************************/
 /***/ ((__unused_webpack_module, exports) => {
 
+"use strict";
 eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.indexTemplate = void 0;\r\nconst indexTemplate = (content) => `\r\n<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <title>Reddit</title>\r\n    <script src=\"static/client.js\" type=\"application/javascript\"></script>\r\n</head>\r\n<body>\r\n    <div id=\"react_root\">${content}</div>\r\n</body>\r\n</html>\r\n`;\r\nexports.indexTemplate = indexTemplate;\r\n\n\n//# sourceURL=webpack://project/./src/server/indexTemplate.js?");
 
 /***/ }),
@@ -26,6 +26,7 @@ eval("\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\n
   \******************************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
+"use strict";
 eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nconst express_1 = __importDefault(__webpack_require__(/*! express */ \"express\"));\r\nconst server_1 = __importDefault(__webpack_require__(/*! react-dom/server */ \"react-dom/server\"));\r\nconst Header_1 = __webpack_require__(/*! ../shared/Header */ \"./src/shared/Header.jsx\");\r\nconst indexTemplate_1 = __webpack_require__(/*! ./indexTemplate */ \"./src/server/indexTemplate.js\");\r\nconst app = (0, express_1.default)();\r\napp.use('/static', express_1.default.static('./dist/client'));\r\napp.get('/', (req, res) => {\r\n    res.send((0, indexTemplate_1.indexTemplate)(server_1.default.renderToString((0, Header_1.Header)())));\r\n});\r\napp.listen(3000, () => {\r\n    console.log('Server started on http:localhost:3000');\r\n});\r\n\n\n//# sourceURL=webpack://project/./src/server/server.js?");
 
 /***/ }),
@@ -36,7 +37,18 @@ eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod)
   \*******************************/
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    var desc = Object.getOwnPropertyDescriptor(m, k);\r\n    if (!desc || (\"get\" in desc ? !m.__esModule : desc.writable || desc.configurable)) {\r\n      desc = { enumerable: true, get: function() { return m[k]; } };\r\n    }\r\n    Object.defineProperty(o, k2, desc);\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.Header = void 0;\r\nconst root_1 = __webpack_require__(/*! react-hot-loader/root */ \"react-hot-loader/root\");\r\nconst React = __importStar(__webpack_require__(/*! react */ \"react\"));\r\nfunction HeaderComponent() {\r\n    return (React.createElement(\"header\", null,\r\n        React.createElement(\"h1\", null, \"Hello, React123\")));\r\n}\r\n;\r\nexports.Header = (0, root_1.hot)(HeaderComponent);\r\n\n\n//# sourceURL=webpack://project/./src/shared/Header.jsx?");
+"use strict";
+eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    var desc = Object.getOwnPropertyDescriptor(m, k);\r\n    if (!desc || (\"get\" in desc ? !m.__esModule : desc.writable || desc.configurable)) {\r\n      desc = { enumerable: true, get: function() { return m[k]; } };\r\n    }\r\n    Object.defineProperty(o, k2, desc);\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.Header = void 0;\r\nconst root_1 = __webpack_require__(/*! react-hot-loader/root */ \"react-hot-loader/root\");\r\nconst React = __importStar(__webpack_require__(/*! react */ \"react\"));\r\nconst header_css_1 = __importDefault(__webpack_require__(/*! ./header.css */ \"./src/shared/header.css\"));\r\nfunction HeaderComponent() {\r\n    return (React.createElement(\"header\", null,\r\n        React.createElement(\"h1\", { className: header_css_1.default.example }, \"Hello, React\")));\r\n}\r\n;\r\nexports.Header = (0, root_1.hot)(HeaderComponent);\r\n\n\n//# sourceURL=webpack://project/./src/shared/Header.jsx?");
+
+/***/ }),
+
+/***/ "./src/shared/header.css":
+/*!*******************************!*\
+  !*** ./src/shared/header.css ***!
+  \*******************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:0)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> .example {\\n|   color: red,\\n| }\");\n\n//# sourceURL=webpack://project/./src/shared/header.css?");
 
 /***/ }),
 
@@ -46,6 +58,7 @@ eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create
   \**************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("express");
 
 /***/ }),
@@ -56,6 +69,7 @@ module.exports = require("express");
   \************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react");
 
 /***/ }),
@@ -66,6 +80,7 @@ module.exports = require("react");
   \***********************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react-dom/server");
 
 /***/ }),
@@ -76,6 +91,7 @@ module.exports = require("react-dom/server");
   \****************************************/
 /***/ ((module) => {
 
+"use strict";
 module.exports = require("react-hot-loader/root");
 
 /***/ })
