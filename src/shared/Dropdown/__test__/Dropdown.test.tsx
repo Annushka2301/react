eb892d8f79f4dc853e3cd02ad0977d140e199bfb
Anna <morozovaannuska@mail.ru>
2022-04-14 21:01:35 +0300

    tests

--- src/shared/Dropdown/__test__/Dropdown.test.tsx
+++ src/shared/Dropdown/__test__/Dropdown.test.tsx
@@ -10,12 +10,12 @@ describe('Dropdown', () => {
     const wrapper = shallow (<Dropdown children={<div/>} button={<button/>} />);
     expect(wrapper).toBeDefined();
     expect(wrapper.find('#button')).toBeDefined();
-    // expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
+    expect(wrapper.find('div.container').isEmptyRender()).toBeFalsy();
   })
 
-  // test('should render (snapshot)', () => {
-  //   const wrapper = shallow (<Dropdown children={<div/>} button={<button/>} />)
+  test('should render (snapshot)', () => {
+    const wrapper = shallow (<Dropdown children={<div/>} button={<button/>} />)
 
-  //   expect(wrapper).toMatchSnapshot();
-  // })
+    expect(wrapper).toMatchSnapshot();
+  })
 })
