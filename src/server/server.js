31fd458d772e4d78eaa717c2c69aabec4154e7ab
Anna <morozovaannuska@mail.ru>
2022-04-18 10:37:49 +0300

    les 8

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { App } from '../App';
 import { Header } from '../shared/Header';
 import { indexTemplate } from './indexTemplate';
+import axios  from 'axios';
 
 const app = express();
 
@@ -28,6 +29,26 @@ app.get ('/', (req, res) => {
       `<div id="react_root">${reactTemplate}</div>`
     )
   )
+});
+
+app.get('/auth', (req, res) => {
+  axios.post(
+    'https://www.reddit.com/api/v1/access_token',
+    `grant_type=authorization_code&code=${req.query.code}&redirect_uri=http://localhost:3000/auth`,
+    {
+      auth: { username: process.env.CLIENT_ID, password: 'wXtg1n292c68LnfgSU9wkpbF0Hdl-g' },
+      headers: { 'Content-type': 'application/x-www-form-urlencoded' }
+    }
+  )
+    .then(({ data }) => {
+      res.send(
+        indexTemplate(ReactDOMServer.renderToString(App()), data['access_token'])
+      )
+    })
+    .catch(console.log)
+  res.send(
+    indexTemplate(ReactDOMServer.renderToString(App())),
+  )
 })
 
 app.get('/auth', (req, res) => {
@@ -35,7 +56,7 @@ app.get('/auth', (req, res) => {
   res.send(
     indexTemplate(ReactDOMServer.renderToString(App()))
   )
-})
+});
 
 app.listen(3000, () => {
   console.log('Server started on http:localhost:3000')
